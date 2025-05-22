bdbd477df051c7ccafdf5abed24457ddd8704c79
root <root@Elyes.>
2025-05-22 10:42:03 +0100

    nodejs

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,12 +2,18 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (line) => {
+  console.log(`Your name is: ${line}`);
+});
+
+rl.on('SIGINT', () => {
+  console.log('This important software is now closing');
+  process.exit();
 });
 
 rl.on('close', () => {
